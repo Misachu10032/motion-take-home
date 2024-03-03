ab71dd0547de3155f03d3c61f003f84092e20e70
Misachu10032 <johnzhou.engineer@gmail.com>
2024-03-02 19:28:11 -0700

    error handling comments

--- utils/handleError.ts
+++ utils/handleError.ts
@@ -13,11 +13,15 @@ interface AxiosError {
 export const handleError = (error: unknown): void => {
     const axiosError = error as AxiosError
     if (axiosError.code === rateLimitErrorCode) {
+    // It will log this specific error message if hits the rate limit
+    // This is specified in the meta doc https://developers.facebook.com/docs/graph-api/overview/rate-limiting/
         console.log('You have hit the rate limit');
     }
     else if (axiosError.message) {
+    // It will log any other axios related error like token invalid etc
         console.log(axiosError.message)
     } else {
+    //It will log all other error
         console.log(error)
     }
 
