da90182fe1c6604b83b660bad9c4b51afdcc7cb3
Misachu10032 <johnzhou.engineer@gmail.com>
2024-03-04 21:55:32 -0700

    variable call speed

--- src/api.ts
+++ src/api.ts
@@ -3,11 +3,14 @@
 import axios from 'axios';
 
 
-interface SuccessResponse {
+export interface SuccessResponse {
     data: {
       id: number;
       name: string;
     };
+  
+    callCount?:number
+  
   }
 
 export const getMetaAPIRequest = async (accessToken:string): Promise<SuccessResponse> => {
@@ -15,8 +18,15 @@ export const getMetaAPIRequest = async (accessToken:string): Promise<SuccessResp
     const apiUrl = `https://graph.facebook.com/v19.0/me?access_token=${accessToken}`;
     try {
       const response = await axios.get(apiUrl);
+      const xAppUsage=JSON.parse(response.headers['x-app-usage']);
+      const callCount = xAppUsage?.call_count;
+      
+      const responseAndHeaders :SuccessResponse={
+        data:response.data,
+        callCount
+      }
 
-      return response.data
+      return responseAndHeaders
 
 
     } catch (error ) {
